68057c157d7980325bee259403c30c87c91aec3f
Vira Murray <vira.avramenko@gmail.com>
2023-03-04 18:14:16 -0500

    ts

--- using-ts.js
+++ using-ts.js
@@ -1,7 +1,6 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1");
-const input2 = document.getElementById("num2");
-
+var button = document.querySelector("button");
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
 function add(num1, num2) {
   return num1 + num2;
   // if (typeof num1 === "number" && typeof num2 === "number") {
@@ -10,7 +9,6 @@ function add(num1, num2) {
   //   return +num1 + +num2;
   // }
 }
-
 button.addEventListener("click", function () {
-  console.log(add(input1.value, input2.value));
+  console.log(add(+input1.value, +input2.value));
 });
